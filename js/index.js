ac3fb9d9ada624697824e95d3c70348f0d2dd589
Nicksonder Examar <nextgendev12@gmail.com>
2019-08-31 00:39:47 -0400

    Continuing tweeking the page

--- js/index.js
+++ js/index.js
@@ -12,16 +12,31 @@ menuIcon[0].addEventListener("click", clickedBtn);
 
 /*******************************************************************/
 
+// Icon menu
 
+var homeIcon = document.getElementById('home-icon');
+var aboutIcon = document.getElementById('about-icon');
+var projectIcon = document.getElementById('project-icon');
 
+// Mobile icon menu
 
+var homeIconMobile = document.getElementById('home-icon');
+var aboutIconMobile = document.getElementById('about-icon');
+var projectIconMobile = document.getElementById('project-icon');
 
+// Pages
+
+var homeSection = document.getElementById('home');
+var aboutSection = document.getElementById('about');
+var projectSection = document.getElementById('project');
 
 /*******************************************************************/
 
 
-var screenResolution = function(mobileScreen) {
-  if (mobileScreen.matches) {
+var screenResolution = function() {
+
+  if (window.matchMedia("(max-width: 900px)").matches) {
+
     // Mobile
 
     //  Making the desktop menu disappear on mobile
@@ -31,7 +46,7 @@ var screenResolution = function(mobileScreen) {
     var clickedTopMobile = function() {
       aboutSection.classList.remove('active');
       projectSection.classList.remove('active');
-      mainMenu.classList.remove('active');
+      // mainMenu.classList.remove('active');
     }
 
     // About section function
@@ -39,7 +54,7 @@ var screenResolution = function(mobileScreen) {
     var clickedAboutMobile = function() {
       aboutSection.classList.add('active');
       projectSection.classList.remove('active');
-      mainMenu.classList.remove('active');
+      // mainMenu.classList.remove('active');
     }
 
     // Project section function
@@ -47,27 +62,19 @@ var screenResolution = function(mobileScreen) {
     var clickedProjectMobile = function() {
       projectSection.classList.add('active');
       aboutSection.classList.remove('active');
-      mainMenu.classList.remove('active');
     }
-
-    // Menu Icon
 
     // Addind Event Listener to the menu icons for mobile
 
     homeIconMobile.addEventListener('click', clickedTopMobile);
     aboutIconMobile.addEventListener('click', clickedAboutMobile);
     projectIconMobile.addEventListener('click', clickedProjectMobile);
+
+
   } else {
-    // Desktop menu
 
+    // Desktop
 
-    // Icon menu
-    var homeIcon = document.getElementById('home-icon');
-    var aboutIcon = document.getElementById('about-icon');
-    var projectIcon = document.getElementById('project-icon');
-    var homeIconMobile = document.getElementById('home-icon');
-    var aboutIconMobile = document.getElementById('about-icon');
-    var projectIconMobile = document.getElementById('project-icon');
 
     // Top section function
 
@@ -80,13 +87,16 @@ var screenResolution = function(mobileScreen) {
 
     var clickedAboutIcon = function() {
       aboutSection.classList.add('active');
+      // aboutSection.style.transition = "transform 1s ease-in-out";
       projectSection.classList.remove('active');
+
     }
 
     // Project section function
 
     var clickedProjectIcon = function() {
       projectSection.classList.add('active');
+      // projectSection.style.transition = "transform 1s ease-in-out";
       aboutSection.classList.remove('active');
     }
 
@@ -96,17 +106,10 @@ var screenResolution = function(mobileScreen) {
     aboutIcon.addEventListener('click', clickedAboutIcon);
     projectIcon.addEventListener('click', clickedProjectIcon);
 
-    // Pages
 
-    var homeSection = document.getElementById('home');
-    var aboutSection = document.getElementById('about');
-    var projectSection = document.getElementById('project');
   }
+
 }
 
 
-var mobileScreen = window.matchMedia("(max-width: 900px)");
-
-screenResolution(mobileScreen);
-
-mobileScreen.addEventListener('click', screenResolution);
+screenResolution();
